0d520c7397b018b8fd8324cb2d70986d3252df97
Leo0o00 <castillofernandezleonardo778@gmail.com>
2024-01-04 16:24:04 -0600

    Program.cs: --Se añadieron los metodos para correr la base de datos en memoria. --Se añadio un endpoint para mostrar un mensaje si se crea la base de datos con exito.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Minimal_API;
+
 var builder = WebApplication.CreateBuilder(args);
+/*Con este metodo creo la base de datos en memoria*/
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+/*EndPoint para mostrar un mensaje si la base de datos es creada con exito*/
+app.MapGet("/dbconexion", ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+
+});
 
 app.Run();
