d7eed632754a301ef4960a2b808ebd1151aaedb8
Leo0o00 <castillofernandezleonardo778@gmail.com>
2024-01-03 21:05:36 -0600

    añadidos los atributos a los campos requeridos en las soluciones -Categoria y -Tarea

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Minimal_API.Models
 {
     public class Categoria
     {
+        [Key]
         public Guid CategoriaId { get; set;}
+        
+        [Required]  //Hace que este campo sea requerido obligatoriamente
+        [MaxLength(150)]  //Establece un maximo de caracteres para este campo
         public String Nombre { get; set; }
         public String Descripcion { get; set; }
         
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace Minimal_API.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
+        
+        [ForeignKey("CategoriaId")] //Esta es una llave foranea ya que pertenece a otra clase
         public Guid CategoriaId { get; set; }
+        
+        [Required]  //Hace que este campo sea requerido obligatoriamente
+        [MaxLength(200)]  //Establece un maximo de caracteres para este campo
         public String Titulo { get; set; }
 
         public Prioridad PrioridadTarea { get; set; }
@@ -17,6 +25,10 @@ namespace Minimal_API.Models
 
         //Con esta linea le asigno a cada tarea una categoria y a su vez puedo solicitar una tarea por su categoria asignada 
         public virtual Categoria Categoria { get; set; }  
+
+
+        [NotMapped]   //Anotacion para evitar que este campo sea almacenado en la base de datos
+        public string Resumen {get; set;}
     }
 
     public enum Prioridad
